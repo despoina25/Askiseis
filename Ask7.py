edf6cda52e0201c23a2ea28d68bdd821fdb4382c
despoina25 <98812980+despoina25@users.noreply.github.com>
2022-02-16 12:53:41 +0200

    Add files via upload

---
+++ Ask7.py
@@ -0,0 +1,16 @@
+f =open ("ascii.txt","r")
+x = f.read()
+f.close()
+print("Τα διαθέσιμα κλειδία είναι:am,vathmos,etos ")
+print("Διάλεξε ένα κλειδί")
+key = input()
+
+z = ('am', 'vatmos', 'etos')
+y = 0
+thisdict = dict.fromkeys(z, y)
+
+
+
+print("Η δημοφιλέστερη τιμή είναι:", pl )
+print("Η μεγαλύτερη τιμή είναι:",max )
+print("Η μικρότερη τιμή  είναι:", min )
